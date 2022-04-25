b57618091e9befe9de1e7f7af58f96caba421f1b
Juan Molina <juan@pcmant.com>
2022-04-25 13:57:50 +0200

    update api

--- PhpApiRestBlogProyectoDaw/routes/services/get.php
+++ PhpApiRestBlogProyectoDaw/routes/services/get.php
@@ -6,5 +6,7 @@ $table = explode('?',$routesArray[3])[0];
 
 $select = $_GET['select'] ?? '*';
 
+if(isset($_GET['linkTo']) && isset($_GET['equalTo']))
+
 $response = new GetController();
 $response->getData($table, $select);
