cf9e132e05cf30be5ea89637871831f4e4b8c8a8
Juan Molina <juan@pcmant.com>
2022-05-23 22:08:54 +0200

    update CORS arreglados de index.php

--- PhpApiRestBlogProyectoDaw/index.php
+++ PhpApiRestBlogProyectoDaw/index.php
@@ -13,9 +13,13 @@ ini_set('error_log', './php_error_log'); /* La ruta del fichero de log de erores
 CORS
 ===============================================*/
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('content-type: application/json; charset=utf-8');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
 
 
 /*==============================================
