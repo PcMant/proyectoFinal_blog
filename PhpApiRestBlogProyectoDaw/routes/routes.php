ac116957a9a14a7b2d870bfa8886b2cae340ad4c
Juan Molina <juan@pcmant.com>
2022-06-20 01:47:16 +0200

    update routes.php

--- PhpApiRestBlogProyectoDaw/routes/routes.php
+++ PhpApiRestBlogProyectoDaw/routes/routes.php
@@ -42,7 +42,7 @@ if (((isset($_GET['framejs']) && $_SERVER['REQUEST_METHOD'] == 'PUT') || $_SERVE
 Cuando no se hace ninguna petición a la API
 ============================================*/
 
-/*Aquí se indica a partir de que endpoint detectar parámetros, siendo 1 el raiz o una carpeta dentro de este
+/*Aquí se indica a partir de que endpoint detectar parámetros, siendo 0 la raiz
 Este es la respuesta que se recibe si se envia una petición sin parámetros, un 404
 */
 if(count($routesArray) == 2){
@@ -62,11 +62,11 @@ Cuando si se tiene una petición a la API
 ========================================*/
 /*
 Aquí se determina a partir de que enpoint de la url se aceptan peticiones.
-Siendo el valor 1 la raiz o un directorio dentro de este
+Siendo el valor 0 la raiz
  */
 if(count($routesArray) > 2 && isset($_SERVER["REQUEST_METHOD"])){
 
-    // Se determina que endpoint se considera la tabla a consultar
+    // Se determina que endpoint se considera la tabla a consultar, siendo 1 desde después de la raiz
     $table = explode('?',$routesArray[3])[0];
 
 	/*=============================================
